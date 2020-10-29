ac925e35a0fa72837a87fe076e490d4edea65638
LeeChan <lee79102@hotmail.com>
2020-10-29 21:15:28 +0800

    fixed: fix edit function

--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -24,13 +24,25 @@ router.delete('/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
-router.put('/:id', (req, res) => {
+router.get('/:id/edit', (req, res) => {
   const id = req.params.id
   return Restaurant.findById(id)
     .lean()
-    .then(restaurant => res.render('edit', { restaurant }))
-    .catch(err => console.log(err))
+    .then((restaurant) => res.render('edit', { restaurant }))
+    .catch(error => console.log(error))
 })
+
+router.put('/:id', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurant => {
+      restaurant = Object.assign(restaurant, req.body)
+      return restaurant.save()
+    })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 
 router.get('/:id', (req, res) => {
   const ID = req.params.id
