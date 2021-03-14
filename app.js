bc99aa72cfa6461909b553458e5c9983d6493d58
LeeChan <lee79102@hotmail.com>
2021-03-14 23:27:22 +0800

    feat: add user login feature vie passport.js

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const methodOverride = require('method-override')
 const routes = require('./routes')
 require('./config/mongoose')
 
+// 載入設定檔，要寫在 express-session 以後
+const usePassport = require('./config/passport')
+
 // require express-handlebars here
 const exphbs = require('express-handlebars')
 
@@ -37,6 +40,10 @@ app.use(
 app.use(express.urlencoded({ extended: true }))
 //body-parser built into Express.js
 app.use(methodOverride('_method'))
+
+
+// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
+usePassport(app)
 
 app.use(routes)
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const router = express.Router()
+// 引用 passport
+const passport = require('passport')
+
 const User = require('../../models/user')
 
-router.get('/login', (req, res) => {
-    res.render('login')
-})
+router.post(
+    '/login',
+    passport.authenticate('local', {
+        successRedirect: '/',
+        failureRedirect: '/users/login',
+    })
+)
 
 // routes/modules/users.js
 router.post('/register', (req, res) => {
