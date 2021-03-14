7dab5a2f586ed89a1b77afbbd04fbfcc894839f4
LeeChan <lee79102@hotmail.com>
2021-03-14 23:01:50 +0800

    fixed: CRUD router feature

--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -8,6 +8,22 @@ router.get('/new', (req, res) => {
   return res.render('new')
 })
 
+router.get('/:id', (req, res) => {
+    const _id = req.params.id
+    return Restaurant.findById(_id)
+        .lean()
+        .then((restaurant) => res.render('show', { restaurant }))
+        .catch((error) => console.error(error))
+})
+
+router.get('/:id/edit', (req, res) => {
+    const _id = req.params.id
+    return Restaurant.findById(_id)
+        .lean()
+        .then((restaurant) => res.render('edit', { restaurant }))
+        .catch((error) => console.error(error))
+})
+
 router.post('/', (req, res) => {
   // 以解構賦值改寫
   const { name, name_en, category, image, location, phone, google_map, rating, description } = req.body
@@ -16,29 +32,25 @@ router.post('/', (req, res) => {
     .catch(error => console.error(error))
 })
 
-router.delete('/:id', (req, res) => {
-  const ID = req.params.id
-  return Restaurant.findById(ID)
-    .then(restaurant => restaurant.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
-
 router.put('/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .lean()
-    .then(restaurant => res.render('edit', { restaurant }))
-    .catch(err => console.log(err))
+    const _id = req.params.id
+    const { name, name_en, category, image, location, phone, google_map, rating, description } = req.body
+    return Restaurant.findById(_id)
+        .then((restaurant) => {
+            restaurant = Object.assign(restaurant, req.body)
+            return restaurant.save()
+        })
+        .then(() => res.redirect(`/restaurants/${_id}`))
+        .catch((err) => console.log(err))
 })
 
-router.get('/:id', (req, res) => {
-  const ID = req.params.id
-  return Restaurant.findById(ID)
-    .lean()
-    .then((restaurant) => res.render('show', { restaurant }))
-    .then((restaurant) => res.render('edit', { restaurant }))
-    .catch(error => console.error(error))
+router.delete('/:id', (req, res) => {
+    const _id = req.params.id
+    return Restaurant.findById(_id)
+        .then((restaurant) => restaurant.remove())
+        .then(() => res.redirect('/'))
+        .catch((error) => console.log(error))
 })
+
 
 module.exports = router
